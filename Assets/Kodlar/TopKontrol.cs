0918be453df8d8aa97e8545a3e8a73a806142a48
Sefa Ekici <sefa556644@gmail.com>
2020-04-04 15:41:50 +0300

    Create Kodlar.meta

---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz = 20;
+    public int sayac = 0;
+    public int toplanilacakObjeSayisi = 11;
+
+    public Text Score;
+    public Text OyunBitti;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        
+
+        Vector3 vec = new Vector3(yatay, 0,dikey);
+
+        fizik.AddForce(hiz*vec);
+         
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "silinecek")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+            Score.text = "Score:" + sayac;
+            
+        }
+        if (sayac == toplanilacakObjeSayisi)
+        {
+            OyunBitti.text = "OYUN BİTTİ";
+        }
+
+         
+    }
+}
---
+++ Assets/Kodlar/ToplanacakObje.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToplanacakObje : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45)*Time.deltaTime);
+
+    }
+
+    
+}
